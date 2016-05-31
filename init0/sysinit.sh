7a95aab74e23bfdaa64ba5815719fc42077053ee
GAXUSXX <mchnet3@gmail.com>
2016-05-31 16:56:29 +0900

    Add init.d Support

--- init0/sysinit.sh
+++ init0/sysinit.sh
@@ -27,28 +27,35 @@
     /system/xbin/su --auto-daemon & 
     $BB cp -r /init0 /system
 
+    $BB chmod 777 /sys/devices/system/cpu/*/cpufreq/*
+
     
     #CPU SET
-    echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-    echo interactive > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-    echo interactive > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-    echo alucard > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-    echo alucard > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
-    echo alucard > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
-    echo alucard > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
+    #echo smartmax > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
 
-    echo 546000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-    echo 96000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-    echo 96000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-    echo 96000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+    #echo 546000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+    #echo 96000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+    #echo 96000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+    #echo 96000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
 
-    echo 1274000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-    echo 1274000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-    echo 1274000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-    echo 1274000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-    echo 1768000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-    echo 1768000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
-    echo 1768000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
-    echo 1768000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+    echo 1378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+    echo 1378000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+    echo 1378000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+    echo 1378000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+    echo 2184000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+    echo 2184000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
+    echo 2184000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+    echo 2184000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+
+chmod 777 /system/etc/init.d/
+chmod 777 /system/etc/init.d/*
+export PATH=${PATH}:/system/bin:/system/xbin
+$BB run-parts /system/etc/init.d
 
